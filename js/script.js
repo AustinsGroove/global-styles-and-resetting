a5f185c705d5abe45f2fed163c9089e7da781975
AustinsGroove <vadimgajiy@gmail.com>
2023-08-26 22:30:20 +0300

    sdfg

--- js/script.js
+++ js/script.js
@@ -1,9 +1,40 @@
-'use strict';
+const atTheOldToad = {
+  potions: [
+    { name: 'Speed potion', price: 460 },
+    { name: 'Dragon breath', price: 780 },
+    { name: 'Stone skin', price: 520 },
+  ],
+  // Change code below this line
+  getPotions() {
+    return this.potions;
+  },
+  addPotion(newPotion) {
+    for (const potion of this.potions) {
+      if (potion.name === newPotion.name) {
+        return `Error! Potion ${newPotion.name} is already in your inventory!`;
+      }
+    }
+    this.potions.push(newPotion);
+  },
+  removePotion(potionName) {
+    for (const potion of this.potions) {
+      if (potion.name === potionName) {
+        this.potions.splice(this.potions.indexOf(potion), 1);
+        return;
+      }
+    }
+    return `Potion ${potionName} is not in inventory!`;
+  },
+  updatePotionName(oldName, newName) {
+    for (const potion of this.potions) {
+      if (potion.name === oldName) {
+        this.potions[this.potions.indexOf(potion)].name = newName;
+        return;
+      }
+    }
+    return `Potion ${oldName} is not in inventory!`;
+  },
+  // Change code above this line
+};
 
-let password = '';
-do {
-  password = prompt('Введите пароль длиннее 4-х символов');
-} while (password.length < 5);
-
-console.log('Ввели пароль: ', password);
-console.log('true' == 'false');
+atTheOldToad.updatePotionName('Speed potion', 'Fast potion');
