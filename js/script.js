5bb0bab32c2c697122fe18dac5f063742669dc5e
AustinsGroove <vadimgajiy@gmail.com>
2023-09-22 09:44:10 +0300

    23

--- js/script.js
+++ js/script.js
@@ -1,33 +1,29 @@
-class Car {
-  // Change code below this line
-  static MAX_PRICE = 50000;
-
-  #price;
-
-  constructor({ price }) {
-    this.#price = price;
+const vehicles = [
+  { make: 'Honda', model: 'CR-V', type: 'suv', price: 24045 },
+  { make: 'Honda', model: 'Accord', type: 'sedan', price: 22455 },
+  { make: 'Mazda', model: 'Mazda 6', type: 'sedan', price: 24195 },
+  { make: 'Mazda', model: 'CX-9', type: 'suv', price: 31520 },
+  { make: 'Toyota', model: '4Runner', type: 'suv', price: 34210 },
+  { make: 'Toyota', model: 'Sequoia', type: 'suv', price: 45560 },
+  { make: 'Toyota', model: 'Tacoma', type: 'truck', price: 24320 },
+  { make: 'Ford', model: 'F-150', type: 'truck', price: 27110 },
+  { make: 'Ford', model: 'Fusion', type: 'sedan', price: 22120 },
+  { make: 'Ford', model: 'Explorer', type: 'suv', price: 31660 },
+];
+class CarsManager {
+  #cars;
+  constructor(carsArr = []) {
+    this.#cars = carsArr;
   }
-
-  get price() {
-    return this.#price;
+  get cars() {
+    return this.#cars;
   }
-
-  set price(newPrice) {
-    if (newPrice > Car.MAX_PRICE) {
-      return;
-    }
-
-    this.#price = newPrice;
+  getModels() {
+    return this.#cars.map(car => car.model);
   }
-  // Change code above this line
 }
-console.log(Car.MAX_PRICE);
+const carsManager = new CarsManager(vehicles);
 
-const audi = new Car({ price: 35000 });
-console.log(audi.price); // 35000
-
-audi.price = 49000;
-console.log(audi.price); // 49000
-
-audi.price = 51000;
-console.log(audi.price); // 49000
+console.log(carsManager);
+console.log(carsManager.cars);
+console.log(carsManager.getModels());
